2432ac256b3372c1a98c153f4714f0089092d820
jaalorsa517 <jaalorsa517@hotmail.com>
2022-06-12 22:17:19 -0500

    Bug: Falla seguridad en attr check de toggle

---
+++ lib/shared/interfaces/index.ts
@@ -0,0 +1,10 @@
+export interface ClassNames {
+  root: string;
+  container: string;
+}
+export interface ITypes {
+  string: Function;
+  number: Function;
+  boolean: Function;
+  object: Function;
+}
