1685e4799d32a0bac9b86a30b337ca2690983c27
Jaime Ortiz <jaalorsa517@hotmail.com>
2023-08-26 21:44:20 -0500

    Se ajusta toggle y actualizacion homepage

--- lib/components/toggle/Toggle.cls.ts
+++ lib/components/toggle/Toggle.cls.ts
@@ -18,20 +18,31 @@ export class Toggle extends ElementAttr {
   constructor() {
     super();
     this._labelOptions = this._getOptionsLabel();
-    this._checked = getType(this.getAttribute("checked") || "false", Types.BOOLEAN, this.shadowDOM);
+    this._checked = getType(
+      this.getAttribute("checked") || "false",
+      Types.BOOLEAN,
+      this.shadowDOM
+    );
     this._templateCls = new ToggleTemplate(this._getLabel);
     this._attrs = this._getLogicAttr();
-    this._eventEmitter = new CustomEvent("change", { detail: { isChecked: this._checked } });
+    this._eventEmitter = new CustomEvent("change", {
+      detail: { isChecked: this._checked },
+    });
   }
   private _getLogicAttr(): ISwitchObject {
     return {
-      checked: (newValue: string) => this._changeChecked(getType(newValue, Types.BOOLEAN, this.shadowDOM)),
+      checked: (newValue: string) =>
+        this._changeChecked(getType(newValue, Types.BOOLEAN, this.shadowDOM)),
       label: (newValue: string) => this._attrLabel(newValue),
     };
   }
   private onClick = (e: MouseEvent) => {
     e.preventDefault();
-    this._checked = !getType(this.getAttribute("checked") || "false", Types.BOOLEAN, this.shadowDOM);
+    this._checked = !getType(
+      this.getAttribute("checked") || "false",
+      Types.BOOLEAN,
+      this.shadowDOM
+    );
     this.setAttribute("checked", `${this._checked}`);
     this.setAttribute("label", this._getLabel);
     this._eventEmitter.detail.isChecked = this._checked;
@@ -47,22 +58,26 @@ export class Toggle extends ElementAttr {
     const radio: HTMLInputElement | null = this.shadowDOM.querySelector(
       `.${this._templateCls.clsNames.radio}`
     );
-    if (radio) {
-      radio.checked = value || false;
-      this._checked = value || false;
-      this._attrs.label(this._getLabel);
-      return;
-    }
+    if (!radio) return;
+    radio.checked = value || false;
+    this._checked = value || false;
+    this._attrs.label(this._getLabel);
   }
   private _changeLabel(label: string) {
-    const labelEl: HTMLElement | null = this.shadowDOM.querySelector(`.${this._templateCls.clsNames.label}`);
+    const labelEl: HTMLElement | null = this.shadowDOM.querySelector(
+      `.${this._templateCls.clsNames.label}`
+    );
     if (labelEl) {
       labelEl.innerText = label;
     }
   }
   private _attrLabel(newValue: string) {
     const hasElements: boolean = newValue?.split("/").length === 2 || false;
-    if (hasElements && (this._labelOptions.length !== 2 || !this._labelOptions.every((value) => value)))
+    if (
+      hasElements &&
+      (this._labelOptions.length !== 2 ||
+        !this._labelOptions.every((value) => value))
+    )
       this._labelOptions = this._getOptionsLabel();
     this._changeLabel(hasElements ? this._getLabel : newValue || "");
   }
@@ -72,6 +87,11 @@ export class Toggle extends ElementAttr {
 
   connectedCallback() {
     this.render(() => {
+      this._checked = getType(
+        this.getAttribute("checked") || "false",
+        Types.BOOLEAN,
+        this.shadowDOM
+      );
       this._changeChecked(this._checked);
       this._eventEmitter.detail.isChecked = this._checked;
       this.dispatchEvent(this._eventEmitter);
